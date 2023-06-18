1eafdd75332d5ff07e7df64d50cad056e4d58c16
PrayagN <Prayag.n.manojnair@gmail.com>
2023-06-18 22:00:34 +0530

    payment stored

--- src/components/User/Payment/PaymentSuccess.jsx
+++ src/components/User/Payment/PaymentSuccess.jsx
@@ -27,7 +27,7 @@ function PaymentSuccess() {
                       <h3 className="md:text-2xl text-base text-gray-900 font-semibold text-center">Payment Done!</h3>
                       <p className="text-gray-600 my-2">Thank you for completing your secure online payment.</p>
                       <p> Have a great day!  </p>
-                     <Link  >you will redirect to your enrollments within {time} second </Link>
+                     <Link  >you will redirect to your bookedEvent within {time} second </Link>
                   </div>
               </div>
           </div>
